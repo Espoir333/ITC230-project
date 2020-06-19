521a75734e5f8fd6ef7aefa7678fe626f311b57d
Espoir Makiese <44072742+Espoir333@users.noreply.github.com>
2020-06-19 01:18:03 -0700

    Update movies.js

--- models/movies.js
+++ models/movies.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 // remote db connection settings. For security, connectionString should be in a separate file not committed to git
- const connectionString = "mongodb+srv://dbuser:nebi123456@cluster0-0tdua.mongodb.net/test?retryWrites=true&w=majority";
+ const connectionString = "mongodb+srv://dbuser:Espoir123456@cluster0-0tdua.mongodb.net/test?retryWrites=true&w=majority";
 
 // local db connection settings 
 // const ip = process.env.ip || '127.0.0.1';
 // const connectionString = 'mongodb://' +ip+ '/<DB_NAME>';
 
-mongoose.connect(connectionString, { dbName: "Itc230", useNewUrlParser: false }); 
+mongoose.connect(connectionString, { dbName: "scc-dbproject", useNewUrlParser: false }); 
 
 mongoose.connection.on('open', () => {
   console.log('Mongoose connected.');
