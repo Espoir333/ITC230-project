8fbf152de7d2f92ad4a35a10cf6dde433fa38622
HN MAk <joysmaks33@gmail.comgit config --global user.name espoir333>
2020-06-20 16:11:34 -0700

    dataaa.js

---
+++ dataaa.js
@@ -0,0 +1,17 @@
+const expect= require("chai").expect;
+const movie=require("../data");
+
+describe("Movie module", () => {
+    it("return requested movie", () => {
+        const result = movie.getltem("titanic");
+        expect(result).to.deep.equal({ "firstName" : "James"
+    });
+    
+    it("fails w/ invalid movie", () => {
+        const result = movie.getltem("fake");
+        expect(result).to.be.undefined;
+    });
+   }); 
+     
+
+
